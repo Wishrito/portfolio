d62eae9a89a21569ad56a5b59902904de1f5721b
Rayan TREMION <contact.rayantremion@gmail.com>
2025-02-27 10:05:13 +0100

    clean v2

--- api/utils.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, Flask
-
-from route_api import api_bp
-
-
-def register_blueprints(app: Flask):
-    app.register_blueprint(api_bp, url_prefix="/api")
--- src/srcipts/script.js
+++
@@ -1,8 +0,0 @@
-let baseURL = "https://github.com/wishrito/"
-function redirect(varName) {
-    if (varName) {
-        window.open(baseURL + varName)
-    } else {
-        window.open(baseURL)
-    }
-}
