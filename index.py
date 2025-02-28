fad928d908ca1261f74ec1b0078d87026ee4dc53
Wishrito <contact.rayantremion@gmail.com>
2025-02-28 15:29:23 +0100

    page about + route + api

--- index.py
+++ index.py
@@ -1,15 +1,10 @@
-from github.GistFile import GistFile
-from flask import Blueprint, request
-import re
 import os
 from pathlib import Path
 
-from flask import Flask, jsonify, redirect, render_template, request
-from github import Github
 import requests
+from flask import Flask, redirect, render_template, request
 
 from routes import api
-
 
 app = Flask(__name__)
 app.template_folder = Path(__file__).parent / "pages"
@@ -17,27 +12,80 @@ app.static_folder = Path(__file__).parent / "src"
 
 app.register_blueprint(api, url_prefix="/api")
 
-# def register_blueprints(app: Flask):
-#     app.register_blueprint(api_bp, url_prefix="/api")
-
 
 @app.errorhandler(404)
 def page_not_found(error):
+    """
+    Handle 404 Page Not Found error.
+
+    Args:
+        error: The error object containing details about the 404 error.
+
+    Returns:
+        A rendered template for the 404 error page.
+    """
     return render_template("404.html")
 
 
 @app.get('/')
 def home():
+    """
+    Renders the home page template.
+
+    Returns:
+        Response: The rendered HTML template for the home page.
+    """
     return render_template("index.html")
 
 
 @app.get('/projects')
-def about():
+def projects():
+    """
+    Renders the projects page.
+
+    Returns:
+        A rendered HTML template for the projects page.
+    """
     return render_template("projects.html")
 
 
+@app.get('/about')
+def about():
+    """
+    Fetches tools data from an API endpoint and renders the 'about' page.
+
+    This function retrieves the root URL from the environment variable 
+    'VERCEL_PROJECT_PRODUCTION_URL'. If the environment variable is not set, 
+    it defaults to the request's root URL. It then fetches data from the 
+    '/api/tools' endpoint and passes this data to the 'about.html' template 
+    for rendering.
+
+    Returns:
+        A rendered HTML template for the 'about' page with tools data.
+    """
+    root_url = os.getenv(
+        "VERCEL_PROJECT_PRODUCTION_URL", "default")
+    if root_url == "default":
+        root_url = request.url_root
+    else:
+        root_url = "https://" + root_url
+    libs_data = requests.get(
+        f"{root_url}/api/tools")
+    return render_template('about.html', tools_data=libs_data.json())
+
 @app.get('/gists')
 def get_gists():
+    """
+    Fetches the list of gists from the specified URL and renders the 'tutorials.html' template with the gists data.
+
+    The function first retrieves the root URL from the environment variable 'VERCEL_PROJECT_PRODUCTION_URL'.
+    If the environment variable is not set, it defaults to using the request's URL root.
+    It then constructs the full URL to fetch the gists metadata and makes a GET request to that URL.
+    Finally, it renders the 'tutorials.html' template with the fetched gists data and the root URL.
+
+    Returns:
+        str: The rendered 'tutorials.html' template with the gists data and root URL.
+    """
     root_url = os.getenv(
         "VERCEL_PROJECT_PRODUCTION_URL", "default")
     if root_url == "default":
@@ -51,6 +99,19 @@ def get_gists():
 
 @app.get('/gist')
 def get_gist():
+    """
+    Fetches gist metadata and renders the tutorials template.
+    This function retrieves the gist ID from the request arguments and constructs
+    the root URL based on the environment variable 'VERCEL_PROJECT_PRODUCTION_URL'.
+    If the environment variable is not set, it defaults to the request's root URL.
+    It then fetches the gist metadata from the constructed URL and renders the
+    'tutorials.html' template with the fetched data.
+    Returns:
+        A redirect to '/gists' if the gist ID is not provided, otherwise renders
+        the 'tutorials.html' template with the gist metadata.
+    Raises:
+        requests.exceptions.RequestException: If there is an issue with the HTTP request.
+    """
     gist_id = request.args.get('id')
     root_url = os.getenv(
         "VERCEL_PROJECT_PRODUCTION_URL", "default")
@@ -61,6 +122,5 @@ def get_gist():
     if not gist_id:
         return redirect('/gists')
 
-    gist_data = requests.get(f"{root_url}/api/gist_metadata?id={gist_id}")
+    gist_data = requests.get(f"{root_url}/api/gist_metadata", {'id': gist_id})
     return render_template('tutorials.html', gist_data=gist_data.json())
-
--- routes.py
+++ routes.py
@@ -1,5 +1,21 @@
+"""This module defines API routes for a Flask application to interact with GitHub gists and retrieve environment metadata.
+Routes:
+    - /tools: Retrieves metadata about the current Python environment and used libraries.
+    - /gist_metadata: Fetches metadata for GitHub gists of a user.
+Functions:
+    - parse_tuto_image(file: GistFile) -> list[str]: Extracts image URLs from the content of a GistFile.
+    - get_env_metadata() -> dict: Retrieves metadata about the current Python environment and used libraries.
+    - get_gist_metadata() -> Union[dict, list]: Fetches metadata for GitHub gists of a user.
+Dependencies:
+    - flask: For creating the API routes.
+    - pygithub: For interacting with the GitHub API.
+    - python-dotenv: For loading environment variables.
+    - requests: For making HTTP requests."""
+
 import os
 import re
+import sys
+from importlib.metadata import version
 from pathlib import Path
 
 from flask import Blueprint, request
@@ -12,14 +28,66 @@ api.static_folder = Path(__file__).parent / "src"
 
 
 def parse_tuto_image(file: GistFile) -> list[str]:
+    """
+    Extracts image URLs from the content of a GistFile.
+
+    Args:
+        file (GistFile): The GistFile object containing the content to parse.
+
+    Returns:
+        list[str]: A list of image URLs found in the content.
+    """
     texte = file.content
     pattern = r"!\[[^\]]+\]\(([^)]+)\)"  # Capture uniquement l'URL
     match = re.findall(pattern, texte)
     return match
 
 
+@api.get("/tools")
+def get_env_metadata():
+    """
+    Retrieves metadata about the current Python environment and used libraries.
+    Returns:
+        dict: A dictionary containing the Python version and a list of dictionaries 
+              with the names and versions of the used libraries.
+    """
+    used_libs = ['flask', 'pygithub', 'python-dotenv', 'requests']
+
+    tools_data = {
+        'python': f'{sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}',
+        'libs': [
+            {
+                'name': lib,
+                'version': version(lib)
+            } for lib in used_libs
+        ]
+    }
+
+    return tools_data
+
+
 @api.get('/gist_metadata')
 def get_gist_metadata():
+    """
+    Fetches metadata for GitHub gists of a user.
+    This function retrieves the metadata of GitHub gists for a user specified by the 
+    environment variables 'GITHUB_TOKEN' and 'GITHUB_USERNAME'. If an 'id' parameter 
+    is provided in the request arguments, it returns the metadata for the specific gist 
+    with that ID. Otherwise, it returns a list of metadata for all gists of the user.
+    Returns:
+        dict: Metadata of a specific gist if 'id' is provided in request arguments.
+        list: List of metadata for all gists if 'id' is not provided in request arguments.
+    Metadata includes:
+        - author: Name of the gist owner.
+        - gist_id: ID of the gist.
+        - description: Description of the gist.
+        - files: List of files in the gist, each containing:
+            - name: Filename.
+            - type: File type.
+            - images: List of image URLs parsed from the file.
+        - embed_url: URL to embed the gist.
+        - title: Title derived from the first file's name.
+    """
     TOKEN = os.getenv('GITHUB_TOKEN')
     USERNAME = os.getenv('GITHUB_USERNAME')
     g = Github(TOKEN)
