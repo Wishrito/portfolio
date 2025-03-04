c81bc828887f9ef92b1b6754ce7aa4e41547625c
Rayan TREMION <contact.rayantremion@gmail.com>
2025-03-04 16:20:24 +0100

    fix: added DOMContentLoaded

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,15 +1,23 @@
-document.getElementById("language_select").addEventListener("change", function () {
-    let selectedLanguage = this.value;
-    let projects = document.getElementsByClassName("card");
+document.addEventListener("DOMContentLoaded", function () {
+    let languageSelect = document.getElementById("language_select");
 
-    for (let project of projects) {
-        let languages = project.dataset.languages.split(', ');
-        if (selectedLanguage === "all") {
-            project.style.display = "block";
-        } else if (languages.includes(selectedLanguage)) {
-            project.style.display = "block";
-        } else {
-            project.style.display = "none";
-        }
+    if (languageSelect) {
+        languageSelect.addEventListener("change", function () {
+            let selectedLanguage = this.value;
+            let projects = document.getElementsByClassName("card");
+
+            for (let project of projects) {
+                let languages = project.dataset.languages.split(', ');
+                if (selectedLanguage === "all") {
+                    project.style.display = "block";
+                } else if (languages.includes(selectedLanguage)) {
+                    project.style.display = "block";
+                } else {
+                    project.style.display = "none";
+                }
+            }
+        });
+    } else {
+        console.log('L\'élément language_select n\'existe pas');
     }
 });
